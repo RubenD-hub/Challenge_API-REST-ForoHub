02297cec5d317de108885851bc2806dc0146f357
Ruben Sanchez Loperena <rubenlop98@gmail.com>
2025-08-19 10:34:15 -0600

    feat: proximas pruebas test y prod

---
+++ src/test/java/ForoHub/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ForoHub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
